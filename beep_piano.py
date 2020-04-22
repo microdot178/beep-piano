c1b3fe0eb80eccf7fa3e7740669c048e04a22d64
noname <noname@android.1.4>
2020-04-22 14:20:36 +0900

    add sound

--- beep_piano.py
+++ beep_piano.py
@@ -9,7 +9,7 @@ root.resizable(False, False)
 
 b1 = Button (text='C', width=3, height=15)
 def play():
-    os.system("beep -f 1500")
+    os.system("beep -f 261.6")
     print(root.geometry())
 b1.config(command=play)
 b1.pack()
@@ -17,7 +17,7 @@ b1.place(x=1, y=2)
 
 b2 = Button (text='C#', width=3, height=15)
 def play2 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 277.2')
 
 b2.config(command=play2)
 b2.pack()
@@ -25,14 +25,14 @@ b2.place(x=50, y=1)
 
 b3 = Button (text='D', width=3, height=15)
 def play3 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 293.7')
 
 b3.config(command=play3)
 b3.pack()
 b3.place(x=100, y=1)
 b4 = Button (text='D#', width=3, height=15)
 def play4 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 311.1')
 
 b4.config(command=play4)
 b4.pack()
@@ -40,7 +40,7 @@ b4.place(x=150, y=1)
 
 b5 = Button (text='E', width=3, height=15)
 def play5 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 329.6')
 
 b5.config(command=play5)
 b5.pack()
@@ -48,7 +48,7 @@ b5.place(x=200, y=1)
 
 b6 = Button (text='F', width=3, height=15)
 def play6 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 349.2')
 
 b6.config(command=play6)
 b6.pack()
@@ -56,7 +56,7 @@ b6.place(x=250, y=1)
 
 b7 = Button (text='F#', width=3, height=15)
 def play7 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 370.0')
 
 b7.config(command=play7)
 b7.pack()
@@ -64,7 +64,7 @@ b7.place(x=300, y=1)
 
 b8 = Button (text='G', width=3, height=15)
 def play8 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 392.0')
 
 b8.config(command=play8)
 b8.pack()
@@ -72,7 +72,7 @@ b8.place(x=350, y=1)
 
 b10 = Button (text='G#', width=3, height=15)
 def play10 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 415.3')
 
 b10.config(command=play10)
 b10.pack()
@@ -80,7 +80,7 @@ b10.place(x=400, y=1)
 
 b11 = Button (text='A', width=3, height=15)
 def play11 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 440.0')
 
 b11.config(command=play11)
 b11.pack()
@@ -88,7 +88,7 @@ b11.place(x=450, y=1)
 
 b12 = Button (text='A#', width=3, height=15)
 def play12 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 466.2')
 
 b12.config(command=play12)
 b12.pack()
@@ -96,7 +96,7 @@ b12.place(x=500, y=1)
 
 b13 = Button (text='B', width=3, height=15)
 def play13 ():
-    os.system('beep -f 1000')
+    os.system('beep -f 493.9')
 
 b13.config(command=play13)
 b13.pack()
